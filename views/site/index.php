ab23a06f6e2e56c3faf71e4e4398c20a242b3744
javokhirergashev <iqtidorziyosi@gmail.com>
2022-08-05 16:15:39 +0500

    header/footer/slider

--- views/site/index.php
+++ views/site/index.php
@@ -5,18 +5,7 @@
 $this->title = 'Region tour';
 ?>
 
-<?= \app\widgets\MainSlider::widget()?>
-<?= \app\widgets\HalfSection::widget()?>
-<?= \app\widgets\SectionVideo::widget()?>
-<?= \app\widgets\Destinations::widget()?>
-<?= \app\widgets\Advert::widget()?>
-<?= \app\widgets\Packages::widget()?>
-<?= \app\widgets\SectionMessage::widget()?>
-<?= \app\widgets\Hotels::widget()?>
-<?= \app\widgets\ToursCategory::widget()?>
-<?= \app\widgets\Testimonial::widget()?>
-<?= \app\widgets\Banner::widget()?>
-<?//= \app\widgets\Blogs::widget()?>
+<?= \app\widgets\Slider::widget()?>
 
 
 
